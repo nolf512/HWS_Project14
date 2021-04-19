5524c383ff7ba432dc6c2591e9f19deb34faab29
nofprog <k.jin3217@gmail.com>
2021-04-19 23:53:14 +0900

    background, gameScore

--- HWS_Project14/GameScene.swift
+++ HWS_Project14/GameScene.swift
@@ -10,9 +10,28 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var gameScore: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
     
     
     override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        addChild(gameScore)
         
         }
     
