3be50ef32da9a7039d5d6785f1feacab7c40bc17
nofprog <k.jin3217@gmail.com>
2021-04-20 22:28:07 +0900

    WhackSlotクラス, 画像の配置

--- HWS_Project14/GameScene.swift
+++ HWS_Project14/GameScene.swift
@@ -10,6 +10,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var slots = [WhackSlot]()
     var gameScore: SKLabelNode!
     
     var score = 0 {
@@ -33,10 +34,25 @@ class GameScene: SKScene {
         gameScore.horizontalAlignmentMode = .left
         addChild(gameScore)
         
+        
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410)) }
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320 )) }
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        
+        
         }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+    }
+    
+    func createSlot(at position: CGPoint){
+        let slot = WhackSlot()
+        slot.configure(at: position)
+        addChild(slot)
+        slots.append(slot)
     }
         
       
