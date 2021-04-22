e72baf2c9c37a12c35b23b2ff930012df7a776c7
nofprog <k.jin3217@gmail.com>
2021-04-22 23:59:25 +0900

    gameOver処理

--- HWS_Project14/GameScene.swift
+++ HWS_Project14/GameScene.swift
@@ -97,8 +97,21 @@ class GameScene: SKScene {
     
     func createEnemy(){
          
+        numRounds += 1
         
-        
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            return
+    
+        }
+            
         popupTime *= 0.991
         
         slots.shuffle()
